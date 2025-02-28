eb583374379b48838d0d7fa70bee8f6635967a64
WonderSi <kirillkozlov00@gmail.com>
2025-02-28 23:45:31 +0700

    create example 1 and 2

---
+++ examples/1/main.cpp
@@ -0,0 +1,48 @@
+//
+// Created by 011ko on 28.02.2025.
+//
+#include <iostream>
+
+using namespace std;
+
+class First {
+public:
+    int x;
+    First(int fx) {
+        x = fx;
+    };
+    int calc() {
+        return x;
+    };
+    void print(string text) {
+        std::cout << text << calc() << std::endl;
+    };
+};
+class Second : public First {
+public:
+    Second(int fx):First(fx) {
+    };
+    int calc() {
+        return x * x;
+    };
+};
+class Third : public Second {
+public:
+    Third(int fx):Second(fx) {
+    };
+    int calc() {
+        return x * x * x;
+    };
+};
+
+//int main() {
+//    First Z(1);
+//    Z.print("class First: ");
+//
+//    Second X(3);
+//    X.print("class Second: ");
+//
+//    Third Y(5);
+//    Y.print("class Third: ");
+//    return 0;
+//};
